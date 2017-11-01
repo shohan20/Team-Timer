f770071c7dacd839a0716000ea29b460554226ff
mehedi <mehedi1055@gmail.com>
2017-11-01 12:26:11 +0600

    feedback.php added

---
+++ php/feedback.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+include 'serverConnection.php';
+$id=$_SESSION['id'];
+$mental_health =$_POST['mental_health'];
+$physical_health=$_POST['physical_health'];
+$productivity=$_POST['productivity'];
+$connection=serverConnect();
+        //store feedback
+$stmt = $connection->prepare("insert into feedback(user_id,mental_health,physical_health,productivity) values(?,?,?,?)") or die("Failed to query database ".mysqli_error($connection));
+$stmt->bind_param('ssss',$id,$mental_health,$physical_health,$productivity);
+$stmt->execute();
+
+?>
--- php/serverConnection.php
+++ php/serverConnection.php
@@ -1,7 +1,7 @@
 <?php
 function serverConnect(){
 	
-	$connection= mysqli_connect("localhost", "root", "abcd");
+	$connection= mysqli_connect("localhost", "root", "");
 	mysqli_select_db($connection,"team-timer");
 	return $connection;
 }
